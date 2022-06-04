26ffa7b77141e6156cdc5ee813df709bbd6fb17a
piotrbullet <piotrbullet69@gmail.com>
2022-06-04 13:18:57 +0200

    porządki

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-import typing
-import subprocess
-import configparser
 import os
-
+import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Activation, Flatten, SpatialDropout2D, Dropout
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, array_to_img
-from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Activation, Flatten, Dropout, Softmax
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.optimizers import SGD
 
 
 def download_playlist():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    playlist_dl_dir = config['MAIN']['playlist_dl_dir']
+    playlist_dl_dir = "D:\\uni\\deepLearning\\dataset_2"
     dl_string = f"spotify_dl -o {playlist_dl_dir}"
     pl_link = input("Enter playlist link: ")
     os.system(dl_string+f" -l {pl_link}")
-
-
-def shorten_audio(folder_dir: str):
-    files = [f for f in os.listdir(folder_dir)]
-
-
-def produce_spectrograms(folder_dir: str):
-    files = [f for f in os.listdir(folder_dir)]
 
 
 def create_dataset(folder_dir):
@@ -67,50 +53,64 @@ def establish_model(unit_numbers: list, pooling_sizes: list, output_size):
 
     for count, n in enumerate(unit_numbers):
         models_no += f"_{n}"
-
         if count == 0:
             model.add(Conv2D(n, pooling_sizes[count], input_shape=(200, 500, 1)))
         else:
             model.add(Conv2D(n, pooling_sizes[count]))
         model.add(MaxPool2D(pool_size=pooling_sizes[count]))
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.3))
 
-    model.add(Activation('relu'))
     model.add(Flatten())
-    model.add(Dense(50))
-    model.add(Dropout(0.2))
-    model.add(Activation('linear'))
+    # model.add(Dropout(0.2))
+    # model.add(Dense(50))
+    # model.add(Activation('sigmoid'))
+    model.add(Dense(20))
+    model.add(Activation('sigmoid'))
     model.add(Dense(output_size))
+    model.add(Softmax())
 
     return model, models_no
 
 
-def train_model(model, x, y, models_no, lr=0.001):
-    model_filepath = "models_2/{}/".format(models_no)
+def train_model(model, x, y, models_no, folder_no, lr=0.001):
+    model_filepath = f"D:\\uni\\deepLearning\\models\\models_{folder_no}\\{models_no}\\"
     model_checkpoint_filepath = model_filepath + '{epoch:02d}-{loss:.7f}-{val_loss:.7f}.hdf5'
-    callbacks = [EarlyStopping(monitor='val_loss', patience=50),
+    callbacks = [EarlyStopping(monitor='val_loss', patience=150),
                  ModelCheckpoint(model_checkpoint_filepath, monitor='loss', save_best_only=True, mode='min')]
-    tf.keras.optimizers.Adam(learning_rate=lr)
-    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
-    model.fit(x, y, validation_split=0.2, epochs=1000, callbacks=callbacks, batch_size=8)
+    optimizer = SGD(lr=lr)
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    history = model.fit(x, y, validation_split=0.2, epochs=1000, callbacks=callbacks, batch_size=8)
+    return history
 
 
 def main():
-    models_set = [[32, 16], [64, 32], [128, 64],
-                  [32, 16, 8], [64, 32, 16], [128, 64, 32],
-                  [32, 16, 8, 4], [64, 32, 16, 8], [128, 64, 32, 16]]
+    models_set = [
+        [16, 8], [32, 16], [64, 32],
+        [16, 8, 4], [32, 16, 8], [64, 32, 16],
+    ]
 
-    pooling_set = [[4, 4], [4, 4], [4, 4],
-                   [4, 2, 2], [4, 2, 2], [4, 2, 2],
-                   [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]
+    pooling_set = [
+        [3, 3], [3, 3], [3, 3],
+        [3, 2, 2], [3, 2, 2], [3, 2, 2],
+    ]
+    folder_no = 5
 
-    general_dir = "D:\\uni\\deepLearning\\musicDataset"
+    general_dir = "D:\\uni\\deepLearning\\dataset_2"
     x_train, x_test, y_train, y_test = create_dataset(general_dir)
     for count, s in enumerate(models_set):
         mdl, mdl_no = establish_model(s, pooling_set[count], 3)
-        train_model(mdl, x_train, y_train, mdl_no, lr=0.0001)
-    # mdl, mdl_no = establish_model([128, 64], [4, 4], 3)
-    # train_model(mdl, x_train, y_train, mdl_no)
+        history = train_model(mdl, x_train, y_train, mdl_no, folder_no, lr=0.0005)
+        model_filepath = f"D:\\uni\\deepLearning\\models\\models_{folder_no}\\{mdl_no}\\"
+        train_acc = mdl.evaluate(x_train, y_train)
+        test_acc = mdl.evaluate(x_test, y_test)
+        with open(model_filepath+'eval.txt', 'w', encoding='utf-8') as f:
+            f.write(f"Train acc: {train_acc}\n"
+                    f"Test acc: {test_acc}")
+        plt.plot(history.history['accuracy'], label='train')
+        plt.plot(history.history['val_accuracy'], label='validation')
+        plt.legend()
+        plt.savefig(f'{model_filepath}val_graph.png')
+        plt.close()
     print("fin")
 
 
