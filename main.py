050ca40f43820641fff67cc8f6318c1c44acb0fe
piotrbullet <piotrbullet69@gmail.com>
2022-06-06 15:34:57 +0200

    idk

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Activation, Flatten, Dropout, Softmax
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Activation, Flatten, Dropout, Softmax, AvgPool2D
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.optimizers import SGD
+# from sklearn.metrics import confusion_matrix
 
 
 def download_playlist():
@@ -61,7 +62,7 @@ def establish_model(unit_numbers: list, pooling_sizes: list, output_size):
         model.add(Dropout(0.3))
 
     model.add(Flatten())
-    # model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
     # model.add(Dense(50))
     # model.add(Activation('sigmoid'))
     model.add(Dense(20))
@@ -83,6 +84,12 @@ def train_model(model, x, y, models_no, folder_no, lr=0.001):
     return history
 
 
+def load_n_predict(model_path: str, x, y):
+    model = keras.models.load_model(model_path)
+    predictions = model.predict(x)
+    return predictions, y
+
+
 def main():
     models_set = [
         [16, 8], [32, 16], [64, 32],
@@ -93,19 +100,23 @@ def main():
         [3, 3], [3, 3], [3, 3],
         [3, 2, 2], [3, 2, 2], [3, 2, 2],
     ]
-    folder_no = 5
+    folder_no = 6
 
     general_dir = "D:\\uni\\deepLearning\\dataset_2"
     x_train, x_test, y_train, y_test = create_dataset(general_dir)
     for count, s in enumerate(models_set):
         mdl, mdl_no = establish_model(s, pooling_set[count], 3)
-        history = train_model(mdl, x_train, y_train, mdl_no, folder_no, lr=0.0005)
+        history = train_model(mdl, x_train, y_train, mdl_no, folder_no, lr=0.0001)
         model_filepath = f"D:\\uni\\deepLearning\\models\\models_{folder_no}\\{mdl_no}\\"
         train_acc = mdl.evaluate(x_train, y_train)
         test_acc = mdl.evaluate(x_test, y_test)
+        predictions = mdl.predict(x_test)
+        # conf_matrix = confusion_matrix(y_test.argmax(axis=1), predictions.argmax(axis=1))
         with open(model_filepath+'eval.txt', 'w', encoding='utf-8') as f:
             f.write(f"Train acc: {train_acc}\n"
-                    f"Test acc: {test_acc}")
+                    f"Test acc: {test_acc}\n"
+                    f"y_test:\n{y_test}\n\n"
+                    f"predictions:\n{predictions}")
         plt.plot(history.history['accuracy'], label='train')
         plt.plot(history.history['val_accuracy'], label='validation')
         plt.legend()
@@ -114,5 +125,20 @@ def main():
     print("fin")
 
 
+def do_statistics(pred, actual_y):
+    pass
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    general_dir = "D:\\uni\\deepLearning\\dataset_2"
+    x_train, x_test, y_train, y_test = create_dataset(general_dir)
+    model_folder_dir = 'D:\\uni\\deepLearning\\models\\models_4'
+    model_dir = 'model_32_16\\311-0.0950032-0.1710008.hdf5'
+    pred, actual_y = load_n_predict(f'{model_folder_dir}\\{model_dir}', x_test, y_test)
+    with open('predicions.txt', 'w', encoding='utf-8') as f:
+        f.write(f'predictions:\n'
+                f'{pred}\n\n'
+                f'actual y:\n'
+                f'{actual_y}')
+    pass
